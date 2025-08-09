be9b1d03c5c8b5c2b86c1defb304b411293012af
ElmurodAzodov <elmurodazodov002@gmail.com>
2025-08-09 13:47:20 +0500

    login and signup

---
+++ typescript/typescript 1/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// index.ts
+let name = "Elmurod";
+let age = 23;
+let plus = name + age;
+console.log("Salom, " + name + "! Yoshingiz: " + age + "da.");
+console.log("Natija: " + plus);
+//# sourceMappingURL=index.js.map
---
+++ typescript/typescript 1/index.ts
@@ -0,0 +1,6 @@
+// index.ts
+let name:string = "Elmurod"
+let age:number = 23
+let plus = name + age
+console.log("Salom, " + name + "! Yoshingiz: " + age + "da.");
+console.log("Natija: " + plus);
